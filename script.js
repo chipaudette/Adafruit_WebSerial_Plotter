cc75a262298422eedb98c33c384442b4553f6cf8
Chip Audette <chipaudette@users.noreply.github.com>
2021-11-17 14:47:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ function logData(line) {
     log.innerHTML += '<span class="timestamp">' + timestamp + ' -> </span>';
     d = null;
   }
-  line.replace(" ","&nbsp"); //added Chip 11/16/2021
+  //line.replace(" ","&nbsp"); //added Chip 11/16/2021
   log.innerHTML += line+ "<br>";
 
   // Remove old log content
